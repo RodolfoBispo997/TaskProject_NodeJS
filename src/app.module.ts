9963acd251f9f0e2d56ae27ff3d5faef3172ebbe
RodolfoBispo997 <RodolfoBispo997@gmail.com>
2026-04-21 12:18:31 -0300

    correção de config prisma + erros de inicialização + conexão prisma com banco neondb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,18 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { ProjectsModule } from './modules/projects/projects.module';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { ProjectsModule } from "./modules/projects/projects.module";
+import { PrismaService } from "./prisma.service";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
-  imports: [ProjectsModule],
+  imports: [
+    ProjectsModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
--- src/modules/projects/projects.service.ts
+++ src/modules/projects/projects.service.ts
@@ -1,25 +1,43 @@
 import { Injectable } from "@nestjs/common";
 import { ProjectRequestDTO } from "./projects.dto";
+import { PrismaService } from "../../prisma.service";
 
 @Injectable()
 export class ProjectsService {
+  constructor(private readonly prisma: PrismaService) {}
+
   findAll() {
-    return ["teste1", "teste2"];
+    return this.prisma.project.findMany();
   }
 
   findById(id: string) {
-    return "teste findById";
+    return this.prisma.project.findFirst({
+      where: {
+        id,
+      },
+    });
   }
 
   create(data: ProjectRequestDTO) {
-    return "create teste1";
+    return this.prisma.project.create({
+      data,
+    });
   }
 
   update(id: string, data: ProjectRequestDTO) {
-    return "update teste1";
+    return this.prisma.project.update({
+      where: {
+        id,
+      },
+      data,
+    });
   }
 
   remove(id: string) {
-    return "remove teste1";
+    return this.prisma.project.delete({
+      where: {
+        id,
+      },
+    });
   }
 }
